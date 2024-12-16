9370aaf031407555c9ee85acbf00cc687aad7bdf
Jonathan Makasare <jrx.2136@gmail.com>
2024-12-16 20:40:37 +0300

    Revert "Add files via upload"

--- Q101.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    FILE *fptr;
-    fptr = fopen("LNMIITSTUDENT.DAT", "a");
-    fprintf(fptr, "%s", "This file is on append Mode");
-    fclose(fptr);
-    return 0;
-}
